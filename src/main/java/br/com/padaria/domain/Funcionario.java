e1ee6cd583da96c2e6d60240e03000fc4fed0fe8
Fabiano Freitas <fabiano.freitas>
2019-10-27 02:36:42 -0300

    Testando primeira classe de dominio Funcionario

---
+++ src/main/java/br/com/padaria/domain/Funcionario.java
@@ -0,0 +1,120 @@
+package br.com.padaria.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+
+public class Funcionario implements Serializable {	
+	private static final long serialVersionUID = 1L;
+	
+	
+	private Integer id;
+	private String nome;
+	private String email;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private Date dataCadastro;
+	private Set<String> telefones = new HashSet<>();
+	
+	public Funcionario() {
+		
+	}
+
+	public Funcionario(Integer id, String nome, String email, Date dataCadastro) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Set<String> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(Set<String> telefones) {
+		this.telefones = telefones;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Funcionario other = (Funcionario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
--- src/main/java/br/com/padaria/resources/FuncionarioResource.java
+++ src/main/java/br/com/padaria/resources/FuncionarioResource.java
@@ -1,16 +1,39 @@
 package br.com.padaria.resources;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.padaria.domain.Funcionario;
 
 @RestController
 @RequestMapping(value = "/funcionarios") //endpointRest
 public class FuncionarioResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST esta legal";
+	public List<Funcionario> listar() throws Exception  {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");		
+		
+		Funcionario f1 = new Funcionario(1, "Maria Flores","maria@email.com",sdf.parse("25/10/2010 10:30"));
+		f1.getTelefones().addAll(Arrays.asList("22645587","969693366"));
+		
+		Funcionario f2 = new Funcionario(2, "Pedro Rocha", "pedro@email.com", sdf.parse("24/12/2011 12:44"));
+		f2.getTelefones().addAll(Arrays.asList("24811588","980003636"));
+		
+		List<Funcionario> lista = new ArrayList<>();
+		lista.add(f1);
+		lista.add(f2);
+		
+		
+		
+		return lista;
 	}
 
 }
