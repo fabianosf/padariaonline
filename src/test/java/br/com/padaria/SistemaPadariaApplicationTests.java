5b4ce834c4ba7b007e80a055f8f877303fb04bad
Fabiano Freitas <fabiano.freitas>
2019-10-27 01:58:48 -0300

    Projeto Criado

---
+++ src/test/java/br/com/padaria/SistemaPadariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.padaria;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPadariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
